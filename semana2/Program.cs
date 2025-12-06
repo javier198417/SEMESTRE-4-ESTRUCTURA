524ae1c5b7d2f802d90f32bca1b58b05a638b6c5
javier198417 <vj.carrenor@uea.edu.ec>
2025-12-06 11:47:07 -0500

    hola

---
+++ semana2/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un círculo con radio 5
+            Circulo c = new Circulo(5);
+            Console.WriteLine("Círculo:");
+            Console.WriteLine("Área: " + c.CalcularArea());
+            Console.WriteLine("Perímetro: " + c.CalcularPerimetro());
+
+            // Crear un rectángulo con base 10 y altura 4
+            Rectangulo r = new Rectangulo(10, 4);
+            Console.WriteLine("\nRectángulo:");
+            Console.WriteLine("Área: " + r.CalcularArea());
+            Console.WriteLine("Perímetro: " + r.CalcularPerimetro());
+        }
+    }
